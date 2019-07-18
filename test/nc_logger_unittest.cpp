53ee0d84af155aafd8b22f25f4d08ba890adcdd0
zhangjunshuai <zhangjs@mailbar.com>
2019-07-18 19:58:22 +0800

    add unitest

--- test/nc_logger_unittest.cpp
+++ test/nc_logger_unittest.cpp
@@ -5,10 +5,34 @@
 
 using namespace ncserver;
 
-TEST(NcLog, basic)
+class NcLogTest : public ::testing::Test
 {
-	NcLog::instance().registerUpdateLogLevelSignal();
-	NcLog::instance().init("echo", LogLevel_info);
-	
-	ASYNC_LOG_ALERT("Hello %s", "world");
+public:
+
+	static void SetUpTestCase()
+	{
+		NcLog::instance().registerUpdateLogLevelSignal();
+		NcLog::instance().init("echo", LogLevel_info);
+	}
+};
+
+TEST_F(NcLogTest, basic)
+{
+	ASYNC_LOG_INFO("basic log %s", "world");
+}
+
+TEST_F(NcLogTest, max)
+{
+	char* text = new char[8 * 1024];
+	memset(text, 'a', 8 * 1024 - 1);
+	text[8 * 1024 - 1] = '\0';
+	ASYNC_LOG_CRIT("max log %s", text);
+}
+
+TEST_F(NcLogTest, superMax)
+{
+	char* text = new char[64 * 1024];
+	memset(text, 'a', 64 * 1024 - 1);
+	text[64 * 1024 - 1] = '\0';
+	ASYNC_LOG_EMERG("superMax log %s", text);
 }
