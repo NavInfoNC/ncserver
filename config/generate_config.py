0b89e1bfd0c06a6a708bc863b3e58d944573a935
zhangjunshuai <zhangjs@mailbar.com>
2019-07-18 17:27:13 +0800

    update

--- config/generate_config.py
+++ config/generate_config.py
@@ -4,8 +4,8 @@ import sys
 import argparse
 
 def generate_config(program_name):
-    logFormat = R'$template logFormat, "[%timegenerated%] [%hostname%] [{\"server\":\"%syslogtag:R:[A-Za-z]\+\[[0-9]\+\]--end%\",\"level\":\"%syslogseverity-text%\",\"log\":\"[%rawmsg%]\"}]\n"'
-    fileFormat = "$template logFormat \"/var/log/%s/%%syslogtag:R:[A-Za-z]\\+--end%%-%%$year%%%%$month%%%%$day%%%%$hour%%.log\"" % (program_name)
+    logFormat = R'$template logFormat, "[%timegenerated%] - %hostname% - %syslogtag:R:[A-Za-z]\+\[[0-9]\+\]--end% - [%rawmsg%]\n"'
+    fileFormat = "$template fileFormat, \"/var/log/%s/%%syslogtag:R:[A-Za-z]\\+--end%%-%%$year%%%%$month%%%%$day%%%%$hour%%.log\"" % (program_name)
     filter = ":syslogtag, ereregex, \"%s\\[[0-9]+\\]\" ?fileFormat;logFormat" % (program_name)
     fileName = "/etc/rsyslog.d/%s.conf" % (program_name)
     fileHandler = open(fileName, "w")
@@ -15,8 +15,8 @@ def generate_config(program_name):
     fileHandler.close()
 
 def main():
-    parser = argparse.ArgumentParser(usage="./generate_config.py $SERVER_NAME", description="Generte rsyslog.d config file (/etc/rsyslog.d/SERVER_NAME.conf) by server name")
-    parser.add_argument("SERVER_NAME", help="assign the server name")
+    parser = argparse.ArgumentParser(usage="./generate_config.py $SERVER_NAME", description="Generte rsyslog.d config file by server name")
+    parser.add_argument("SERVER_NAME", help="assign the server name", type=str)
     try:
         args = parser.parse_args()
     except:
