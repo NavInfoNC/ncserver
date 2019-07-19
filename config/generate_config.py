50241be072f83e7b4c4cb831fc8afe7ad6fb3536
张军帅 <497585690@qq.com>
2019-07-19 20:01:10 +0800

    fixed #8 修复linux环境下日志编译错误问题 (#9)
    修复日志输出格式和修复linux环境下ASYNC_LOG_INFO("Hello world");日志编译错误问题
    fixed #8

--- config/generate_config.py
+++ config/generate_config.py
@@ -4,8 +4,8 @@ import sys
 import argparse
 
 def generate_config(program_name):
-    logFormat = R'$template logFormat, "[%timegenerated%] - %hostname% - %syslogtag:R:[A-Za-z]\+\[[0-9]\+\]--end% - [%rawmsg%]\n"'
-    fileFormat = "$template fileFormat, \"/var/log/%s/%%syslogtag:R:[A-Za-z]\\+--end%%-%%$year%%%%$month%%%%$day%%%%$hour%%.log\"" % (program_name)
+    logFormat = R'$template logFormat, "[%timegenerated%] - %hostname% - %syslogtag:R:[A-Za-z_-]\+\[[0-9]\+\]--end% - %rawmsg%\n"'
+    fileFormat = "$template fileFormat, \"/var/log/%s/%%syslogtag:R:[A-Za-z_-]\\+--end%%.log.%%$year%%%%$month%%%%$day%%%%$hour%%\"" % (program_name)
     filter = ":syslogtag, ereregex, \"%s\\[[0-9]+\\]\" ?fileFormat;logFormat" % (program_name)
     fileName = "/etc/rsyslog.d/%s.conf" % (program_name)
     fileHandler = open(fileName, "w")
